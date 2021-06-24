f1cb786b748c47446ab38fec644b71d68903b046
Janindu-Pathirana <78893460+Janindu-Pathirana@users.noreply.github.com>
2021-06-24 19:51:15 +0530

    my edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,15 +11,83 @@ void main() {
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: Dices(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+var num = 1;
+var num2 = 1;
+
+void randNum() {
+  var random = new Random();
+  num = random.nextInt(6) + 1;
+  num2 = random.nextInt(6) + 1;
+}
+
+class Dices extends StatefulWidget {
+  @override
+  _DicesState createState() => _DicesState();
+}
+
+class _DicesState extends State<Dices> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.only(left: 8.0, right: 8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset(
+                      "images/dice$num.png",
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset(
+                      "images/dice$num2.png",
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    randNum();
+                  });
+                },
+                child: Card(
+                  margin: EdgeInsets.only(left: 80, right: 80),
+                  child: ListTile(
+                    title: Center(
+                      child: Text(
+                        "Press to roll",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
